c666792c4a6adbc97799d74827be7ec8c7671cc1
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2022-02-03 18:11:13 +0100

    Add files via upload
    too much recursion
    wanted to use switch function

--- scriptShowHideList.js
+++ scriptShowHideList.js
@@ -6,21 +6,21 @@ var ExpProSectionStyle = document.getElementById("Experiences");
 var showExpProSection = document.getElementById("ExpProSection");
 var showExpPersoSection = document.getElementById("ExpPersoSection");
 
+function breakShowExpPro() {
+  activeTab = 1;
+  switchTab();
+}
 function ShowExpPro() {
   if (showExpProSection.style.display === "none" && showExpPersoSection.style.display === "none") {
     showExpProSection.style.display = "block";
     showExpPersoSection.style.display = "block";
-    activeTab = 1
-  } else if (activeTab != 1){
-    showExpProSection.style.display = "none";
-    showExpPersoSection.style.display = "none";
   } else {
     showExpProSection.style.display = "none";
     showExpPersoSection.style.display = "none";
   }
 }
 
-ExpProSectionStyle.setAttribute("onclick", "ShowExpPro()");
+ExpProSectionStyle.setAttribute("onclick", "breakShowExpPro()");
 
 // --
 
@@ -32,19 +32,20 @@ ExpProSectionStyle.setAttribute("onclick", "ShowExpPro()");
 var competencesSectionStyle = document.getElementById("Competences");
 var showListSkillsSection = document.getElementById("listSkills");
 
+function breakShowCompetences() {
+  activeTab = 2;
+  switchTab();
+}
 
 function ShowCompetences() {
   if (showListSkillsSection.style.display === "none") {
     showListSkillsSection.style.display = "block";
-    activeTab = 2;
-  } else if (activeTab != 2) {
-    showListSkillsSection.style.display = "none";
   } else {
     showListSkillsSection.style.display = "none";
   }
 }
 
-competencesSectionStyle.setAttribute("onclick", "ShowCompetences()");
+competencesSectionStyle.setAttribute("onclick", "breakShowCompetences()");
 
 // --
 
@@ -56,19 +57,20 @@ competencesSectionStyle.setAttribute("onclick", "ShowCompetences()");
 var aProposSectionStyle = document.getElementById("APropos");
 var showAProposDescrSection = document.getElementById("AProposDescr");
 
+function breakShowAPropos() {
+  activeTab = 3;
+  switchTab();
+}
 
 function ShowAPropos() {
   if (showAProposDescrSection.style.display === "none") {
     showAProposDescrSection.style.display = "block";
-    activeTab = 3;
-  } else if (activeTab != 3) {
-    showAProposDescrSection.style.display = "none";
   } else {
     showAProposDescrSection.style.display = "none";
   }
 }
 
-aProposSectionStyle.setAttribute("onclick", "ShowAPropos()");
+aProposSectionStyle.setAttribute("onclick", "breakShowAPropos()");
 
 
 // --
@@ -81,19 +83,21 @@ aProposSectionStyle.setAttribute("onclick", "ShowAPropos()");
 var formationsSectionStyle = document.getElementById("Formations");
 var showListCoursesSection = document.getElementById("listCourses");
 
+function breakShowFormations() {
+  activeTab = 4;
+  switchTab();
+}
+
 
 function ShowFormations() {
   if (showListCoursesSection.style.display === "none") {
     showListCoursesSection.style.display = "block";
-    activeTab = 4;
-  } else if (activeTab != 4) {
-    showListCoursesSection.style.display = "none";
   } else {
     showListCoursesSection.style.display = "none";
   }
 }
 
-formationsSectionStyle.setAttribute("onclick", "ShowFormations()");
+formationsSectionStyle.setAttribute("onclick", "breakShowFormations()");
 
 
 // --
@@ -106,19 +110,80 @@ formationsSectionStyle.setAttribute("onclick", "ShowFormations()");
 var languesSectionStyle = document.getElementById("Langues");
 var showListLanguesSection = document.getElementById("listLangues");
 
+function breakShowLangues() {
+  activeTab = 5;
+  switchTab();
+}
 
-function ShowLangues() {
+function showLangues(){
   if (showListLanguesSection.style.display === "none") {
     showListLanguesSection.style.display = "block";
-    activeTab = 5;
-  } else if (activeTab != 5) {
-    showListLanguesSection.style.display = "none";
   } else {
     showListLanguesSection.style.display = "none";
   }
 }
 
-languesSectionStyle.setAttribute("onclick", "ShowLangues()");
+languesSectionStyle.setAttribute("onclick", "breakShowLangues()");
 
 
 // --
+
+function switchTab() {
+  if ((activeTab == 1)) {
+    breakShowExpPro();
+    showListSkillsSection.style.display = "none";
+    showAProposDescrSection.style.display = "none";
+    showListCoursesSection.style.display = "none";
+    showListLanguesSection.style.display = "none";
+  }
+  if ((activeTab == 2)) {
+    breakShowCompetences();
+    showExpProSection.style.display = "none";
+    showExpPersoSection.style.display = "none";
+    showAProposDescrSection.style.display = "none";
+    showListCoursesSection.style.display = "none";
+    showListLanguesSection.style.display = "none";
+  }
+  if ((activeTab == 3)) {
+    breakShowAPropos();
+    showExpProSection.style.display = "none";
+    showExpPersoSection.style.display = "none";
+    showListSkillsSection.style.display = "none";
+    showListCoursesSection.style.display = "none";
+    showListLanguesSection.style.display = "none";
+  }
+  if ((activeTab == 4)) {
+    breakShowFormations();
+    showExpProSection.style.display = "none";
+    showExpPersoSection.style.display = "none";
+    showListSkillsSection.style.display = "none";
+    showAProposDescrSection.style.display = "none";
+    showListLanguesSection.style.display = "none";
+  }
+  if ((activeTab == 5)) {
+    breakShowLangues();
+    showExpProSection.style.display = "none";
+    showExpPersoSection.style.display = "none";
+    showListSkillsSection.style.display = "none";
+    showAProposDescrSection.style.display = "none";
+    showListCoursesSection.style.display = "none";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
